96916861bac38f2318980e2a57c04441ada470ce
David Rafael Tejada Mercado <david.tejada@intec.edu.do>
2024-04-26 12:13:29 -0400

    Add project files.

---
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+using WebApiAutores.Entidades;
+using WebApiAutores.Filtros;
+using WebApiAutores.Servicios;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    //[Authorize]
+
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDbContex context;
+        private readonly IServicio servicio;
+        private readonly ServicioTransient servicioTransient;
+        private readonly ServicioScoped servicioScoped;
+        private readonly ServicioSingleton servicioSingleton;
+        private readonly ILogger<AutoresController> logger;
+
+        public AutoresController(ApplicationDbContex context, IServicio servicio,
+            ServicioTransient servicioTransient, ServicioScoped servicioScoped,
+            ServicioSingleton servicioSingleton, ILogger<AutoresController> logger)
+        {
+            this.context = context;
+            this.servicio = servicio;
+            this.servicioTransient = servicioTransient;
+            this.servicioScoped = servicioScoped;
+            this.servicioSingleton = servicioSingleton;
+            this.logger = logger;
+        }
+        [HttpGet] // api/autores
+        [HttpGet("listado")] // api/autores/listado
+        [HttpGet("/listado")] // listado
+        //[ResponseCache(Duration = 5)]
+        [ServiceFilter(typeof(MiFiltroDeAccion))]
+        public async Task<ActionResult<List<Autor>>> Get()
+        {
+            throw new NotImplementedException();
+            logger.LogInformation("Estamos obteniendo los autores");
+            logger.LogWarning("Este es un mensaje Warning de prueba");
+            servicio.RealizarTarea();
+            return await context.Autores.Include(x => x.Libros).ToListAsync();
+        }
+
+        [HttpGet("GUID")]
+        //[ResponseCache(Duration = 5)]
+        public ActionResult ObtenerGuids()
+        {
+            return Ok(new
+            {
+                AutoresController_Transient = servicioTransient.Guid,
+                ServicioA_Transient = servicio.ObtenerTransient(),
+                AutoresController_Scoped = servicioScoped.Guid,
+                ServicioA_Scoped = servicio.ObtenerScoped(),
+                AutoresController_Singleton = servicioSingleton.Guid,
+                ServicioA_Singleton = servicio.ObtenerSingleton(),
+            });
+        }
+
+        [HttpGet("primero")]
+        public async Task<ActionResult<Autor>> PrimerAutor()
+        {
+            return await context.Autores.FirstOrDefaultAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Autor>> Get(int id)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Id == id);
+            if (autor == null) 
+            {
+                return NotFound();
+            }
+            return autor;
+        }
+
+        [HttpGet("{nombre}")]
+        public async Task<ActionResult<Autor>> Get(string nombre)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Nombre.Contains(nombre));
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            return autor;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Autor autor)
+        {
+            var existeAutorConElMismoNombre = await context.Autores.AnyAsync(x => x.Nombre == autor.Nombre);
+           
+            if(existeAutorConElMismoNombre)
+            {
+                return BadRequest($"Ya existe el autor con el nombre de {autor.Nombre}");
+            }
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")] // api/autores/{id}
+
+        public async Task<ActionResult> Put(Autor autor, int id)
+        {
+            if(autor.Id !=  id)
+            {
+                return BadRequest($"El id {id} no coincide con el id de la URL");
+            }
+
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+
+        public async Task<ActionResult> Delete(int id)
+        {
+
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+            if (!existe) 
+            {
+                return NotFound();
+            }
+            context.Remove(new Autor() { Id = id });
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ WebApiAutores/Startup.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using WebApiAutores.Controllers;
+using WebApiAutores.Filtros;
+using WebApiAutores.Middlewares;
+using WebApiAutores.Servicios;
+
+namespace WebApiAutores
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            //var autorController = new AutoresController(
+            //    new ApplicationDbContex(null),
+            //        new ServicioA(new Logger())
+            //    );
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers(opciones => 
+            { 
+                opciones.Filters.Add(typeof(FiltroDeExcepcion)); 
+            }).AddJsonOptions(x => 
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+            services.AddDbContext<ApplicationDbContex>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
+
+            services.AddTransient<IServicio, ServicioA>();
+
+            services.AddTransient<ServicioTransient>();
+            services.AddScoped<ServicioScoped>();
+            services.AddSingleton<ServicioSingleton>();
+            services.AddTransient<MiFiltroDeAccion>();
+            services.AddHostedService<EscribirEnArchivo>();
+
+            services.AddResponseCaching();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        {
+            app.UserLoguearRespuestaHTTP();
+
+
+
+            app.Map("/ruta1", app =>
+                app.Run(async contexto =>
+            await contexto.Response.WriteAsync("Estoy interceptando la tuberia"))
+            );
+        
+
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseResponseCaching();
+
+            app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
